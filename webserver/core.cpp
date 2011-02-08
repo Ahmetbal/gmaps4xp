2aa69d1b33ec5042c519a7fa36b3913349f5f533
mariocavicchi <mariocavicchi@22615946-5cd7-11de-b7a8-4be610c0df87>
2011-02-08 12:11:41 +0000

    Now It works... I have to integrate...

--- webserver/core.cpp
+++ webserver/core.cpp
@@ -1,11 +1,23 @@
 #include "core.h"
+//#define MAIN
 
 
+#ifndef MAIN
 XPLMDataRef gGroundSpeed;
 XPLMDataRef gPlaneHeading;
 XPLMDataRef gPlaneLat;
 XPLMDataRef gPlaneLon;
 XPLMDataRef gPlaneAlt;
+#else
+int main(){
+	pthread_t thread;
+	int *x = (int *)malloc(sizeof(int));
+        pthread_create(&thread, NULL, webServer, (void *)NULL);
+	pthread_join(thread,(void **)x);
+
+	return 0;
+}
+#endif
 
 void *webServer(void *arg){
 	int 			sock;
@@ -23,12 +35,13 @@ void *webServer(void *arg){
 	listen(sock, 5);
 
 
+	#ifndef MAIN
 	gGroundSpeed    = XPLMFindDataRef("sim/flightmodel/position/groundspeed");
 	gPlaneHeading   = XPLMFindDataRef("sim/flightmodel/position/psi");
 	gPlaneLat       = XPLMFindDataRef("sim/flightmodel/position/latitude");
 	gPlaneLon       = XPLMFindDataRef("sim/flightmodel/position/longitude");
 	gPlaneAlt       = XPLMFindDataRef("sim/flightmodel/position/elevation");
-
+	#endif
 
 
 	printf("HTTP server listening on port %d\n", PORT );
@@ -39,6 +52,7 @@ void *webServer(void *arg){
 		f = fdopen(s, "r+");
 		process(f);
 		fclose(f);
+		close(s);
 
 	}
 
@@ -47,29 +61,33 @@ void *webServer(void *arg){
 }
 
 
-void send_headers(FILE *f, int status, const char *title, const char *extra, const char *mime, int length){
+void send_headers(FILE *f, int status, const char *title, const char *mime, int length){
 	time_t	now;
 	char	timebuf[128];
 
 	now = time(NULL);
 	strftime(timebuf, sizeof(timebuf), RFC1123FMT, gmtime(&now));
 	
-			fprintf(f, "%s %d %s\n",		PROTOCOL, status, title);
+	if (status!=505)fprintf(f, "%s %d %s\n",		PROTOCOL, status, title);
+	else		fprintf(f, "HTTP/1.0 %d %s\n",		status, title);
+
 			fprintf(f, "Date: %s\r\n",		timebuf);
 			fprintf(f, "Server: %s\r\n",		SERVER);
+			fprintf(f, "Last-Modified: %s\r\n",	timebuf);
+			fprintf(f, "Accept-Ranges: bytes\r\n");
 			fprintf(f, "Keep-Alive: timeout=15, max=100\r\n");
 			fprintf(f, "Connection: Keep-Alive\r\n");
 	if (mime)	fprintf(f, "Content-Type: %s\r\n", 	mime);
 	if (length >= 0)fprintf(f, "Content-Length: %d\r\n", 	length);
-	if (extra)	fprintf(f, "%s\r\n", 			extra);
+			fprintf(f, "Access-Control-Allow-Origin: http://members.ferrara.linux.it\r\n"); 
 			fprintf(f, "\r\n");
 	
 
 }
 
 
-void send_error(FILE *f, int status, const char *title, const char *extra, const char *text){
-	send_headers(f, status, title, extra, "text/html", -1);
+void send_error(FILE *f, int status, const char *title, const char *text){
+	send_headers(f, status, title, "text/html", -1);
 	fprintf(f, "<html><head><title>%d %s</title></head>\r\n", status, title);
 	fprintf(f, "<body><h4>%d %s</h4>\r\n", status, title);
 	fprintf(f, "%s\r\n", text);
@@ -89,17 +107,20 @@ void send_file(FILE *f, char *path){
 	
 	bzero(data, 4095);
 
+	#ifndef MAIN
         Latitude	= XPLMGetDataf(gPlaneLat);
         Longitude	= XPLMGetDataf(gPlaneLon);
         Altitude	= XPLMGetDataf(gPlaneAlt);
         Heading		= XPLMGetDataf(gPlaneHeading);
         Speed           = XPLMGetDataf(gGroundSpeed);
+	#endif
 
 	sprintf(data,"<infos><plane latitude=\"%f\" longitude=\"%f\" altitude=\"%f\" heading=\"%f\" speed=\"%f\" /></infos>", Latitude, Longitude, Altitude, Heading, Speed);
 	length = strlen(data);
 
-	send_headers(f, 200, "OK", NULL, "text/xml", length);
-	fwrite(data, length, 1, f);
+	send_headers(f, 200, "OK", "text/xml", length);
+	printf("Sent: %d\n", fwrite(data, 1, length, f));
+	fflush(f);
 
 }
 
@@ -110,6 +131,8 @@ int process(FILE *f){
 	char 		*protocol	= NULL;
 	int		i		= 0;
 
+	//start:
+
 	while (1){
 		if ( ( buf[i] = (char *)malloc(sizeof(char) * 4096) ) == NULL )	return -1;
 		bzero(buf[i], 4095);
@@ -117,6 +140,9 @@ int process(FILE *f){
 		if (( buf[i][0] == '\r' ) && ( buf[i][1] == '\n' )) break;
 		i++;
 	}
+
+	printf("%s", buf[0]);
+
 	
 	method		= strtok(buf[0], 	" ");
 	path		= strtok(NULL, 		" ");
@@ -129,10 +155,12 @@ int process(FILE *f){
 	fseek(f, 0, SEEK_CUR); 
 
 
-	if 	(strcmp(method,  "GET") 	!= 0)	send_error(f, 501, "Not supported",  	NULL, "Method is not supported.");
-	else if (strcmp(protocol, PROTOCOL) 	!= 0)	send_error(f, 404, "Not Found",  	NULL, NULL);
+	if 	(strcmp(method,  "GET") 	!= 0)	send_error(f, 501, "Not supported",  			 "Method is not supported.");
+	else if (strcmp(protocol, PROTOCOL) 	!= 0)	send_error(f, 505, "HTTP Version Not Supported",  	 NULL);
 	else						send_file(f, path);
 	
+	//goto start;
+
 	return 0;
 
 }
