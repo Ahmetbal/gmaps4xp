a76e7c4c01c4560e39bbb1ef9d674f324601fc4d
mariocavicchi <mariocavicchi@22615946-5cd7-11de-b7a8-4be610c0df87>
2011-04-20 14:08:08 +0000

    Good point to do a commit

--- trunk/docs/visulDownload.sh
+++ trunk/docs/visulDownload.sh
@@ -1,63 +1,55 @@
 #!/bin/bash
 
+TILE_LEVEL[2]="65536"
+TILE_LEVEL[4]="32768"	
+TILE_LEVEL[8]="16384"
+TILE_LEVEL[16]="8192"	
+TILE_LEVEL[32]="4096"	
+TILE_LEVEL[64]="2048"	
+TILE_LEVEL[128]="1024"
+TILE_LEVEL[256]="512"
+TILE_LEVEL[512]="256"
+TILE_LEVEL[1024]="128"
+TILE_LEVEL[2048]="64"
+TILE_LEVEL[4096]="32"
+TILE_LEVEL[8192]="16"
+TILE_LEVEL[16384]="8"
+TILE_LEVEL[32768]="4"
+TILE_LEVEL[65536]="2"
+
+
+
 
 
 downloadTile(){
 	xcoord="$1"
 	ycoord="$2"
-	file="$3"
-	wget -q "http://visualimages1.paginegialle.it/xmlvisual.php/europa.imgi?cmd=tile&format=png&x=${xcoord}&y=${ycoord}&z=32&extra=2&ts=256&q=100&rdr=0&sito=visual&v=1" -O "$file"
-}
-
-downloadTileZ(){
-	xcoord="$1"
-	ycoord="$2"
 	zcoord="$3"
 	file="$4"
-	wget -q "http://visualimages1.paginegialle.it/xmlvisual.php/europa.imgi?cmd=tile&format=png&x=${xcoord}&y=${ycoord}&z=${zcoord}&extra=2&ts=256&q=100&rdr=0&sito=visual&v=1" -O "$file"
+	server="$[ ( $xcoord % 4 )  + 1 ]"
+	wget -q "http://visualimages${server}.paginegialle.it/xmlvisual.php/europa.imgi?cmd=tile&format=png&x=${xcoord}&y=${ycoord}&z=${zcoord}&extra=2&ts=256&q=100&rdr=0&sito=visual&v=1" -O "$file"
 }
 
 # http://visualimages3.paginegialle.it/xml.php/europa-orto.imgi?cmd=tile&format=jpeg&x=1&y=1&z=32768&extra=2&ts=256&q=65&rdr=0&sito=visual
 # http://visualimages3.paginegialle.it/xmlvisual.php/europa.imgi?cmd=tile&format=jpeg&x=8422&y=10628&z=8&extra=2&ts=256&q=60&rdr=0&sito=visual&v=1
-
 # http://visualimages1.paginegialle.it/xml.php/europa-orto.imgi?cmd=tile&format=jpeg&x=8432&y=10648&z=8&extra=2&ts=256&q=65&rdr=0&sito=visual
+ 
+getXY(){
+	lat="$1"
+	lon="$2"
+	zoom="$3"
+	zone="$( echo "scale = 6; ( $lon   + 180   ) / 6 + 1" | bc )"
+	zone="${zone%.*}"
 
  
-# 16384 x 16384
-# 
-# 
-# 2	65536	1x1
-# 4	32768	
-# 8	16384	20
-# 16	8192	
-# 32	4096	
-# 64	2048	
-# 128	1024
-# 256	512
-# 512	256
-# 1024	128
-# 2048	64
-# 4096	32
-# 8192	16
-# 16384	8
-# 32768	4
-# 
 
-cat << EOM | bc -l
-
-
+cat << EOM | bc -l | awk -F. {'print $1'} | tr "\n" " "
 mapwidthlevel1pixel 	= 33554432
 mapwidthmeters		= 4709238.7
 mapcentreutmeasting	= 637855.35
 mapcentreutmnorthing	= 5671353.65
-
-
-
-
 define tan(x) { x = s(x) / c(x); return (x); }
-
-define latlongutm(e,a,t){
-
+define latlongxy(e,a,t){
 	p = 6378137
 	n = 0.00669438
 	l = 0.9996
@@ -73,61 +65,51 @@ define latlongutm(e,a,t){
 	j = p*((1-n/4-3*n*n/64-5*n*n*n/256)*f-(3*n/8+3*n*n/32+45*n*n*n/1024) * s(2*f)+(15*n*n/256+45*n*n*n/1024) * s(4*f)-(35*n*n*n/3072)*s(6*f))
 	s = (l*g*(o+(1-d+m)*o*o*o/6+(5-18*d+d*d+72*m-58*h)*o*o*o*o*o/120)+500000)
 	r = (l*(j+g*tan(f)*(o*o/2+(5-d+9*m+4*m*m)*o*o*o*o/24+(61-58*d+d*d+600*m-330*h)*o*o*o*o*o*o/720)));
-
-
 	x = s
 	y = r
-
 	n = mapwidthmeters
 	f = mapcentreutmeasting
 	g = mapcentreutmnorthing
 	j = t
-
 	e = x+((n/2)-f)
 	b = g + (n/2) - y
 	l = e / n
-	h = b / n;
-          
-	l
-	h
+	h = b / n
+	l * ${TILE_LEVEL[$zoom]}
+	h * ${TILE_LEVEL[$zoom]}
 
+}
+
+a = latlongxy($lon, $lat, $zone)
+
+EOM
 
 }
 
 
+UL=( 44.854227 11.597803 )
+LR=( 44.824209 11.636779 )
+LEVEL="8"
 
-a = latlongutm(11.617954, 44.811688, 32)
+
+ULxy=( $( getXY ${UL[*] } $LEVEL ) )
+LRxy=( $( getXY ${LR[*] } $LEVEL ) )
 
 
-EOM
-echo "
-x = 8432
-y = 10648
-z = 8
 
-"
-
-exit 0
-
-# http://visualimages1.paginegialle.it/xml.php/europa-orto.imgi?cmd=tile&format=jpeg&x=8432&y=10648&z=8&extra=2&ts=256&q=65&rdr=0&sito=visual
-xoffset="0"
-yoffset="0"
-
-pi="$( echo "scale=32; 4*a(1)" | bc -l )"
-
-lat="44.811688"
-lon="11.617954"
+xsize="$[ ${LRxy[0]} - ${ULxy[0]} ]"
+ysize="$[ ${LRxy[1]} - ${ULxy[1]} ]"
+xoffset="${ULxy[0]}"
+yoffset="${ULxy[1]}"
 
 cnt="0"
-for y in $( seq 0  1 ) ; do
+for y in $( seq 0  $ysize ) ; do
 	yT="$[ $yoffset + $y ]"
-	for x in $( seq 0  1 ) ; do
+	for x in $( seq 0  $xsize ) ; do
 		xT="$[ $xoffset + $x ]"
 
 		echo "${xT} ${yT}"
-		downloadTileZ  "$xT" "$yT" 65536  "tmp-${xT}-${yT}.png"
-		# downloadTileZ  "$xT" "$yT" 32768 "tmp-${xT}-${yT}.png"
-		# downloadTile   "$xT" "$yT" "tmp-${xT}-${yT}.png"
+		downloadTile "${xT}" "${yT}" "$LEVEL" "tmp-${xT}-${yT}.png"
 		convert -page +$[ 256 * $x  ]+$[ 256 * $y ] "tmp-${xT}-${yT}.png" -format PNG32 "tile-${xT}-${yT}.png"
 		imageList[$cnt]="tile-${xT}-${yT}.png"
 		rm -f "tmp-${xT}-${yT}.png"
