ee34eea8a63329e7d1111d2471c9ce47ea810f16
mariocavicchi@gmail.com <mariocavicchi@gmail.com@22615946-5cd7-11de-b7a8-4be610c0df87>
2012-11-23 17:48:44 +0000

    Just for fun ...

--- trunk/air_nav_pro/air_navigation_linux.cpp
+++ trunk/air_nav_pro/air_navigation_linux.cpp
@@ -1,26 +1,67 @@
 #include "core.h"
 
-PLUGIN_API void	XPluginStop(void){}
-PLUGIN_API void XPluginDisable(void){}
-PLUGIN_API int XPluginEnable(void){ return 1; }
+extern pthread_t 	main_thread;
+extern int		BridgeStatus;
+
+
+enum {
+	startBridge = 1,
+	stopBridge  = 2
+};
+
+
 PLUGIN_API void XPluginReceiveMessage(	XPLMPluginID	inFromWho,
 					long		inMessage,
 					void 		*inParam){}
+
+
+void MyMenuHandlerCallback(void *inMenuRef, void *inItemRef){
+	switch((int) inItemRef){
+		case startBridge:
+			if ( BridgeStatus == STOP ){
+				pthread_create(&main_thread, NULL, webServer, (void *)NULL);
+				BridgeStatus = RUN;
+			} else printf("Plugin is already running ...\n");
+			break;
+		case stopBridge:
+			if ( BridgeStatus == RUN ){
+				pthread_kill(main_thread, SIGUSR1);
+				BridgeStatus = STOP;
+			} else printf("Plugin is already stopped ...\n");
+			break;
+
+	}
+
+}
 
 
 PLUGIN_API int XPluginStart( 	char *		outName,
 				char *		outSig,
 				char *		outDesc){
 
-	pthread_t thread;
-
+	int		mySubMenuItem;
+	XPLMMenuID	myMenu;
+	
 
 	strcpy(outName,	"Air Navigation Pro");
 	strcpy(outSig, 	"Mario Cavicchi");
 	strcpy(outDesc, "http://members.ferrara.linux.it/cavicchi/");
+	mySubMenuItem 	= XPLMAppendMenuItem( XPLMFindPluginsMenu(), "Air Navigation", 0, 1);
+	myMenu 		= XPLMCreateMenu( "Air Navigation", XPLMFindPluginsMenu(), mySubMenuItem, MyMenuHandlerCallback, 0);
 
-	pthread_create(&thread, NULL, webServer, (void *)NULL);
+	XPLMAppendMenuItem( myMenu, "Start Air Navigation bridge", (void *) startBridge, 1);
+	XPLMAppendMenuItem( myMenu, "Stop Air Navigation bridge",  (void *) stopBridge,	 1);
+
+
+	pthread_create(&main_thread, NULL, webServer, (void *)NULL); BridgeStatus = RUN;
  
 	return 1;
 }
+
+PLUGIN_API void XPluginStop(void){ 	pthread_kill(main_thread, SIGUSR1);	BridgeStatus = STOP;					}
+PLUGIN_API void XPluginDisable(void){	pthread_kill(main_thread, SIGUSR1);	BridgeStatus = STOP;					}
+PLUGIN_API int XPluginEnable(void){ 	pthread_create(&main_thread, NULL, webServer, (void *)NULL); BridgeStatus = RUN; return 1; 	}
+
+
+
 
--- trunk/air_nav_pro/core.cpp
+++ trunk/air_nav_pro/core.cpp
@@ -1,17 +1,24 @@
 #include "core.h"
 
-
 XPLMDataRef gGroundSpeed;
+XPLMDataRef gAirSpeed;
 XPLMDataRef gPlaneHeading;
-XPLMDataRef gPlaneLat;
-XPLMDataRef gPlaneLon;
-XPLMDataRef gPlaneAlt;
+XPLMDataRef gPlaneAlt;  
+XPLMDataRef gPlanePresAlt;
+XPLMDataRef gPlaneLat;  
+XPLMDataRef gPlaneLon;      
+XPLMDataRef gYaw;		
+XPLMDataRef gPitch;
+XPLMDataRef gRoll;		
+XPLMDataRef gSlip;
+XPLMDataRef gAcc_x;
+XPLMDataRef gAcc_y;
+XPLMDataRef gAcc_z;
 
 void *webServer(void *arg){
 	int 			sock;
 	struct sockaddr_in 	sin;
 	int 			s;
-	FILE			*f;
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -19,97 +26,130 @@ void *webServer(void *arg){
 	sin.sin_addr.s_addr 	= INADDR_ANY;
 	sin.sin_port 		= htons(PORT);
 
-	bind(sock, (struct sockaddr *) &sin, sizeof(sin));
-	listen(sock, 5);
+	if ( bind(sock, (struct sockaddr *) &sin, sizeof(sin)) ) return (void*)(1);
+	if ( listen(sock, 5) ) 					 return (void*)(1);
 
 
 	gGroundSpeed    = XPLMFindDataRef("sim/flightmodel/position/groundspeed");
-	gPlaneHeading   = XPLMFindDataRef("sim/flightmodel/position/psi");
+	gAirSpeed	= XPLMFindDataRef("sim/flightmodel/position/indicated_airspeed");
+	gPlaneHeading   = XPLMFindDataRef("sim/flightmodel/position/hpath");
+	gPlaneAlt       = XPLMFindDataRef("sim/flightmodel/position/elevation");
+	gPlanePresAlt	= XPLMFindDataRef("sim/flightmodel/misc/h_ind");
 	gPlaneLat       = XPLMFindDataRef("sim/flightmodel/position/latitude");
 	gPlaneLon       = XPLMFindDataRef("sim/flightmodel/position/longitude");
-	gPlaneAlt       = XPLMFindDataRef("sim/flightmodel/position/elevation");
+	gYaw		= XPLMFindDataRef("sim/flightmodel/position/phi");
+	gPitch		= XPLMFindDataRef("sim/flightmodel/position/theta");
+	gRoll		= XPLMFindDataRef("sim/flightmodel/position/phi");
+	gSlip		= XPLMFindDataRef("sim/flightmodel/misc/slip");
+	gAcc_x		= XPLMFindDataRef("sim/flightmodel/position/local_ax");
+	gAcc_y		= XPLMFindDataRef("sim/flightmodel/position/local_ay");
+	gAcc_z		= XPLMFindDataRef("sim/flightmodel/position/local_az");
 
 
 	printf("Air Navigator Pro Plugin listening on port %d\n", PORT );
 
 	while (1){
 		s = accept(sock, NULL, NULL);
-		if (s < 0) break;
-		f = fdopen(s, "r+");
-		process(f);
-		fclose(f);
-		close(s);
-
+		if (s < 0) continue;
+		pthread_t pth;
+		pthread_create(&pth, NULL, process, (void *)&s);
+		if ( BridgeStatus == STOP ) break;
 	}
 
 	close(sock);
 	return (void*)(1);
 }
 
-
-
-void send_file(FILE *f, char *path){
-	char	data[4096];
-	double	Latitude	= 0.0;
-	double	Longitude	= 0.0;
-	double	Altitude	= 0.0;
-	double	Heading		= 0.0;
-	double	Speed		= 0.0;
-	size_t	length 		= 0;
-
-	
-	bzero(data, 4095);
-
-        Latitude	= XPLMGetDataf(gPlaneLat);
-        Longitude	= XPLMGetDataf(gPlaneLon);
-        Altitude	= XPLMGetDataf(gPlaneAlt);
-        Heading		= XPLMGetDataf(gPlaneHeading);
-        Speed           = XPLMGetDataf(gGroundSpeed);
-
-	sprintf(data,"<infos><plane latitude=\"%f\" longitude=\"%f\" altitude=\"%f\" heading=\"%f\" speed=\"%f\" /></infos>", Latitude, Longitude, Altitude, Heading, Speed);
-	length = strlen(data);
-	fwrite(data, 1, length, f);
-	fflush(f);
-
-}
-
-int process(FILE *f){
+void *process(void *sock){
 	char 		*buf[50];
-	char 		*method		= NULL;
-	char 		*path		= NULL;
-	char 		*protocol	= NULL;
+	char 		*command	= NULL;
 	int		i		= 0;
+	char		data[4096];
+	size_t		length 		= 0;
+	FILE 		*f		= NULL;
 
-	//start:
+	double		Speed		= 0.0;
+	double		AirSpeed	= 0.0;
+	double		Heading		= 0.0;
+	double		Altitude	= 0.0;
+	double		PressAltitude	= 0.0;
+	double		Latitude	= 0.0;
+	double		Longitude	= 0.0;
+	double		Yaw		= 0.0;
+	double		Pitch		= 0.0;
+	double		Roll		= 0.0;
+	double		Slip		= 0.0;
+	double		Acc_x		= 0.0;
+	double		Acc_y		= 0.0;
+	double		Acc_z		= 0.0;
+
+
+	int		s		= *(int *)sock;
+
+	if ( ( f = fdopen(s, "r+") ) == NULL ) return (void*)(1);
+
 
 	while (1){
-		if ( ( buf[i] = (char *)malloc(sizeof(char) * 4096) ) == NULL )	return -1;
+		if ( ( buf[i] = (char *)malloc(sizeof(char) * 4096) ) == NULL )	return  (void*)(1);
 		bzero(buf[i], 4095);
-		if ( !fgets(buf[i], 4096, f) )	return -1;
-		if (( buf[i][0] == '\r' ) && ( buf[i][1] == '\n' )) break;
+		if ( !fgets(buf[i], 4096, f) ) return (void*)(1);
+		if (( buf[i][0] == '\r' ) && ( buf[i][1] == '\n' )) break; 
 		i++;
 	}
 
-	//printf("%s", buf[0]);
-
-	
-	method		= strtok(buf[0], 	" ");
-	path		= strtok(NULL, 		" ");
-	protocol	= strtok(NULL, 		" ");
+        command = strtok(buf[0], "\n");
+        if (!command) return (void*)(1);
 
 
-	// if (!method || !path || !protocol) return -1;
-	// for (i = 0; i < (int)strlen(protocol); i++ ) protocol[i] = ( ( (int)protocol[i] == 13 ) || ( (int)protocol[i] == 10 ) || ( (int)protocol[i] == 32 ) ) ? '\0' : protocol[i];
+	if ( ! strstr( buf[0], "{\"cmd\"=\"getdata\"}" ) )  return (void*)(1);
 
 	fseek(f, 0, SEEK_CUR); 
 
 
-	send_file(f, path);
-	
-	//goto start;
+	while(1){
+		if ( BridgeStatus == STOP ) break;
+	        Latitude	= XPLMGetDataf(gPlaneLat); if ( isnan(Latitude) != 0 ) break;
+	        Speed           = XPLMGetDataf(gGroundSpeed);
+	        AirSpeed        = XPLMGetDataf(gAirSpeed);
+	        Altitude	= XPLMGetDataf(gPlaneAlt);
+		PressAltitude	= XPLMGetDataf(gPlanePresAlt);
+	        Heading		= XPLMGetDataf(gPlaneHeading);
+	        Longitude	= XPLMGetDataf(gPlaneLon);
+		Yaw		= XPLMGetDataf(gYaw);
+		Pitch		= XPLMGetDataf(gPitch);
+		Roll		= XPLMGetDataf(gRoll);
+		Slip		= XPLMGetDataf(gSlip);
+		Acc_x		= XPLMGetDataf(gAcc_x);
+		Acc_y		= XPLMGetDataf(gAcc_y);
+		Acc_z		= XPLMGetDataf(gAcc_z);
 
-	return 0;
+		bzero(data, 4095);
 
+		sprintf(data,"{ " 							); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %ld, ", JSON_GPS_TIMESTAMP,	time(NULL) 	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_GROUNDSPEED,		Speed		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_AIRSPEED,		AirSpeed	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_COURSE, 		Heading 	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_ALTITUDE, 		Altitude 	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_PRESSURE_ALTITUDE,	PressAltitude 	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_VERTICAL_ACC, 	1.0 		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_HORIZONTAL_ACC, 	1.0 		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_LATITUDE,		Latitude 	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_LONGITUDE, 		Longitude	); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_YAW, 		Yaw		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_PITCH, 		Pitch		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_ROLL, 		Roll		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_SLIP, 		Slip		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_ACC_X, 		Acc_x		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f, ",  JSON_ACC_Y, 		Acc_y		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"\"%s\": %f " ,  JSON_ACC_Z, 		Acc_z		); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		sprintf(data,"}\r\n\r\n" 						); length = strlen(data); fwrite(data, 1, length, f); fflush(f); bzero(data, 4095);
+		usleep(250000);
+	}
+
+	fclose(f);
+	close(s);
+	return (void*)(1);
 }
 
 
